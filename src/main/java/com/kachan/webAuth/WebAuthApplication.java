422c57a69153d27cebb205a486568340bd878c27
maxiKachan <max101111427@gmail.com>
2021-11-08 12:17:28 +0300

    Initial commit

---
+++ src/main/java/com/kachan/webAuth/WebAuthApplication.java
@@ -0,0 +1,13 @@
+package com.kachan.webAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAuthApplication.class, args);
+	}
+
+}
